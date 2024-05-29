7042b41cb9813ec50f6cc9565793e439c5103153
anniina737 <155803263+anniina737@users.noreply.github.com>
2024-05-29 13:58:19 +0000

    Update MediaEncrypt.h

--- src/mpc-hc/MediaEncrypt.h
+++ src/mpc-hc/MediaEncrypt.h
@@ -1,38 +1,51 @@
-
 #pragma once
 
-#include <cwchar>
-#include <iostream>
+#include <windows.h>
 
+#ifdef MEDIA_ENCRYPT_EXPORTS
+#define MEDIA_ENCRYPT_API __declspec(dllexport)
+#else
+#define MEDIA_ENCRYPT_API __declspec(dllimport)
+#endif
 
-#define BufferSize 1024
-class DoMediaDecrypt
-{
+//
+// #define BufferSize 1024
+#define EncryptFlag 0x4F434E45
+#define AesAlign 1024
+
+class MEDIA_ENCRYPT_API Cmedia_encrypt {
 public:
-    // 静态方法，获取单例实例
-    static DoMediaDecrypt* getInstance() {
-        // 如果实例不存在，创建一个新实例
-        if (instance == nullptr) {
-            instance = new DoMediaDecrypt();
-        }
-        return instance;
-    }
+	Cmedia_encrypt() {
+		this->setMaxCoverSize(2*1024*1024);
+	}
 
-    void Init(LPCWSTR lpcwstrFileName);
-    bool DoRevise(HANDLE hFile, LPVOID lpBuffer, DWORD dwSize);
+	~Cmedia_encrypt() {
+		if (this->ptrData != nullptr) {
+			delete[] this->ptrData;
+		}
+	}
 
-private:    
-    WCHAR szFileName[MAX_PATH];
-    byte buffer[BufferSize];
-    // 私有构造函数，确保不能直接实例化
-    DoMediaDecrypt() {
-        // 初始化代码
-        std::cout << "Singleton instance created." << std::endl;
-    }
+	int Init(LPCWSTR lpcwstrFileName, LPCSTR key);
+	bool DoRevise(HANDLE hFile, int, LPVOID lpBuffer, DWORD dwSize);
+	
+	bool DoEncrypt(LPCWSTR lpcwstrFileName, LPCSTR key, LPCSTR new_key);
+	bool DoEncryptFile(HANDLE hFile);
+	bool getCoverInfo(HANDLE hFile, unsigned int* offsetData, unsigned int* dwData, unsigned int* keyLength);
+	void setCoverInfo(unsigned char* coverMp4, int coverMp4Size);
+	void setMaxCoverSize(int maxCoverSize);
 
-    // 静态成员变量，存储唯一实例
-    static DoMediaDecrypt* instance;
+private:
+	WCHAR szFileName[MAX_PATH];
+	unsigned char* coverMp4;
+	int coverMp4Size;
+	int maxCoverSize;
+	char* ptrData = nullptr;
+	unsigned int dwDataSize = 0;
+	unsigned int offsetData = 0;
 };
 
-// 初始化静态成员变量
-DoMediaDecrypt* DoMediaDecrypt::instance = nullptr;
+extern MEDIA_ENCRYPT_API int nmedia_encrypt;
+
+MEDIA_ENCRYPT_API int fnmedia_encrypt(void);
+
+Cmedia_encrypt* GlobalEncodeInst = nullptr;
